223a3821de312a43e5859c88cacb5912bec83d7c
yusuf abdulsamad <yusufabdulsamad93@gmail.com>
2023-07-25 11:59:41 +0100

    done

---
+++ server.js
@@ -0,0 +1,106 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https')
+const express = require('express');
+const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session')
+require('dotenv').config();
+
+const PORT = 3000;
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+}
+
+const AUTH_OPTIONS ={
+    callbackURL: '/auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+}
+
+function verifyCallback(accessToken, refreshToken, profile, done){
+    console.log('Google profile', profile);
+    done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+
+//save the session to cookie
+passport.serializeUser((user, done)=>{
+done(null, user.id);
+});
+//read the session to cookie
+passport.deserializeUser((id, done)=>{
+    // User.findById(id).then(user =>{
+    //     done(null, user);
+    // })
+    done(null, id);
+});
+
+const app = express();
+
+app.use(helmet())
+
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys:[config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+function checkLogin(req, res, next){
+    console.log('Current user is', req.user)
+    const isLogin = req.isAuthenticated() && req.user;
+    if(!isLogin) {
+        return res.status(401).json({
+            error: 'You must log in'
+        });
+    }
+    next();
+ }
+
+app.get('/auth/google',
+passport.authenticate('google',{
+    scope:['email'],
+}));
+
+app.get('/auth/google/callback',
+ passport.authenticate('google',{
+    failureRedirect: 'failure',
+    successRedirect:'/',
+    
+}),
+(req, res) =>{
+    console.log('Google called us back!');
+}
+);
+app.get('/auth/logout', (req, res)=>{
+    req.logout()
+    return res.redirect('/')
+})
+
+app.get('/secret', checkLogin, (req, res)=>{
+  return  res.send('your secret value number is 1!');
+});
+
+app.get('/failure', (req, res)=>{
+    return res.send('failed to log in!');
+});
+
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,'public', 'index.html'));
+});
+
+https.createServer({
+    key:fs.readFileSync('key.pem'),
+    cert:fs.readFileSync('cert.pem'),
+},app).listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}...`)
+});
